feed042bf1b51a32b42e3cba050a9787aabf4c38
David J. Brenes <davidjbrenes@gmail.com>
2013-12-22 12:52:44 +0100

    Included coauthors information

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,5 +4,9 @@ class Author < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   has_and_belongs_to_many :papers
+  has_many :coauthors, through: :papers, uniq: true, source: :authors
 
+  def coauthors
+    super - [self]
+  end
 end
