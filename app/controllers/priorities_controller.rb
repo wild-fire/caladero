4c3bbba23bb1dda51001334517555d9bdf239ced
David J. Brenes <davidjbrenes@gmail.com>
2013-12-22 10:58:25 +0100

    Ordering papers by the score

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -8,7 +8,7 @@ class PrioritiesController < ApplicationController
 
     render_404 unless Paper.priority.values.include? @priority
 
-    @papers_with_categories = Paper.includes(:category).prioritized_as(params[:id]).group_by(&:category)
+    @papers_with_categories = Paper.includes(:category).prioritized_as(params[:id]).order(score: :desc).group_by(&:category)
 
   end
 
