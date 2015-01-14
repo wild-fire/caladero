916082ca4b783c4870a8b5d5a242b4940bf2dbec
David J. Brenes <davidjbrenes@gmail.com>
2015-01-14 20:40:14 +0100

    Checking that a category has unread papers before ordering it

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -8,7 +8,14 @@ class PrioritiesController < ApplicationController
 
     render_404 unless Paper.priority.values.include? @priority
 
-    @papers_with_categories = Paper.includes(:category).prioritized_as(params[:id]).order(score: :desc).group_by(&:category).sort_by{|category, papers| papers.reject(&:read).map(&:score).max }.reverse
+    @papers_with_categories = Paper.includes(:category). # We get all the papers wit its category
+      prioritized_as(params[:id]). # Filter for the current priority
+        order(score: :desc). # Order them by score
+        group_by(&:category). # Then group by category
+        sort_by do |category, papers| # and sort
+          unread_papers = papers.reject(&:read)
+          unread_papers.blank? ? -1 : unread_papers.map(&:score).max # We sort the categories for the maximum score of its unread papers (or -1 if there's no unread paper)
+        end.reverse # and finally reverse the sort
 
   end
 
